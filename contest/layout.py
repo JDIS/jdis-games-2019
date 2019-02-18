dcd1cf912f70c632f81ced61ec270502ee8abe02
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2019-02-18 16:08:53 -0500

    Python 3 conversion

--- contest/layout.py
+++ contest/layout.py
@@ -16,6 +16,7 @@ from util import manhattanDistance
 from game import Grid
 import os
 import random
+from functools import reduce
 
 VISIBILITY_MATRIX_CACHE = {}
 
@@ -66,11 +67,11 @@ class Layout:
         return self.walls[x][col]
 
     def getRandomLegalPosition(self):
-        x = random.choice(range(self.width))
-        y = random.choice(range(self.height))
+        x = random.choice(list(range(self.width)))
+        y = random.choice(list(range(self.height)))
         while self.isWall( (x, y) ):
-            x = random.choice(range(self.width))
-            y = random.choice(range(self.height))
+            x = random.choice(list(range(self.width)))
+            y = random.choice(list(range(self.height)))
         return (x,y)
 
     def getRandomCorner(self):
