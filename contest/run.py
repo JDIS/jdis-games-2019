94aa2868210a6a4878497fc3378ede2b67c28459
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2019-06-28 00:55:52 -0400

    Added arguments for run.py

--- contest/run.py
+++ contest/run.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
+import argparse
 from pacman.capture import *
 import pacman.captureGraphicsDisplay as captureGraphicsDisplay
-from bots import baselineTeam
 import pacman.layout as layout
-import MyBot
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Helper file to run a bot")
+    parser.add_argument("--redBot", type=str, help='Module name of the red bot', default="MyBot")
+    parser.add_argument("--blueBot", type=str, help='Module name of the blue bot', default="MyBot")
+    parser.add_argument("--layout", type=str, help='Name of the layout (in the layouts directory or "random"', default="random")
+    parser.add_argument("--numGames", type=int, help='Number of games to be played', default=1)
+    return parser.parse_args()
 
 if __name__ == '__main__':
+    args = parse_args()
+    lay = layout.Layout(randomLayout().split('\n')) if args.layout == "random" else layout.getLayout(args.layout)
+    redBot = __import__(args.redBot).createTeam
+    blueBot = __import__(args.blueBot).createTeam
+
     display = captureGraphicsDisplay.PacmanGraphics("Red", "Blue", 1, 0, capture=True)
-    agents = sum([list(el) for el in zip(MyBot.createTeam(0, 2, True), baselineTeam.createTeam(1, 3, False))],[])
+    agents = sum([list(el) for el in zip(redBot(0, 2, True), blueBot(1, 3, False))],[])
 
     options = {
-        'layouts': [layout.Layout(randomLayout().split('\n'))], # for fixed layout: layout.getLayout("defaultCapture")
+        'layouts': [lay] * args.numGames,
         'agents': agents,
         'display': display,
         'length': 1000,
-        'numGames': 1,
+        'numGames': args.numGames,
         'record': True,
         'numTraining': 0,
         'redTeamName': 'My team',
