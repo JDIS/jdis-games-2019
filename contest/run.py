dc290eefb89f2addfa4a8cc1a9dbd1732f38b0da
AntoineTheb <theberge.antoine.cem@gmail.com>
2019-06-29 14:30:05 -0400

    Fix seed

--- contest/run.py
+++ contest/run.py
@@ -8,14 +8,15 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Helper file to run a bot")
     parser.add_argument("--redBot", type=str, help='Module name of the red bot', default="MyBot")
     parser.add_argument("--blueBot", type=str, help='Module name of the blue bot', default="MyBot")
-    parser.add_argument("--layout", type=str, help='Name of the layout (in the layouts directory or "random"', default="random")
+    parser.add_argument("--layout", type=str, help='Name of the layout (in the layouts directory or "random"', default="RANDOM")
     parser.add_argument("--numGames", type=int, help='Number of games to be played', default=1)
     parser.add_argument("--gameLength", type=int, help='Length of a game', default=1000)
+    parser.add_argument('--seed', type=str, help='Seed to set randomness', default=None)
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
-    lay = layout.Layout(randomLayout().split('\n')) if args.layout == "random" else layout.getLayout(args.layout)
+
     redBot = __import__(args.redBot).createTeam
     blueBot = __import__(args.blueBot).createTeam
 
@@ -23,7 +24,6 @@ if __name__ == '__main__':
     agents = sum([list(el) for el in zip(redBot(0, 2, True), blueBot(1, 3, False))],[])
 
     options = {
-        'layouts': [lay] * args.numGames,
         'agents': agents,
         'display': display,
         'length': args.gameLength,
@@ -33,10 +33,10 @@ if __name__ == '__main__':
         'redTeamName': args.redBot,
         'blueTeamName': args.blueBot,
         'muteAgents': False,
-        'catchExceptions': False 
+        'catchExceptions': False,
+        'seed': args.seed,
+        'layout': args.layout
     }
 
     games = runGames(**options)
     print('Score: %d' % games[0].state.data.score)
-
-     
